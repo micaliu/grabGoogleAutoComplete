e5dfdbeceff63f86e8aed25c39cfbca3153014a3
Collin Meyers <cfmeyers@gmail.com>
2014-06-04 01:51:55 -0400

    Ensured works as UNIX utility, updated readme

--- README.md
+++ README.md
@@ -4,3 +4,16 @@ grabGoogleAutoSuggest
 Command line script to get keywords from Google Autosuggest given search term.
 
 Modeled on https://www.youtube.com/watch?v=Z5MmeV6aeYE
+
+Built like a UNIX command line utility.  For a file test.tx that contains keywor (one keyword/phrase per line):
+
+~~~
+cat test.txt | python grabGoogleAutoSuggest.py
+~~~
+
+Or to get keywords individually:
+
+~~~
+echo "kitchen" | python grabGoogleAutoSuggest.py
+~~~
+
--- grabGoogleAutoSuggest.py
+++ grabGoogleAutoSuggest.py
@@ -1,1 +1,34 @@
 #! /usr/local/bin/python
+
+from urllib import urlopen
+from string import ascii_lowercase
+import re
+import sys
+
+
+suggestURL = "http://google.com/complete/search?output=toolbar&q="
+
+def grabSuggestions(key):
+    
+    keywords = []
+
+    for char in ascii_lowercase:
+        webpage = urlopen(suggestURL+key+" "+char).read()
+        keywords.extend(re.findall(r'data="(\w*\s*\w*\s*\w*\s*)"', webpage))
+
+    return keywords
+
+
+
+if __name__ == "__main__":
+
+    #usage should be along lines of
+    #cat test.txt | python grabGoogleAutoSuggest.py
+
+    for key in sys.stdin.readlines():
+        key = key.strip()
+        keywords = grabSuggestions(key)
+
+        for word in keywords:
+            print word
+
